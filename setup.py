3fcee03a16b17d599ac91e8af81054c07d9513f3
Diarmuid Collins <dcollins@curtisswright.com>
2014-03-14 14:39:00 +0000

    Added the setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AcraNetwork',
+      version='0.1',
+      description='Acra Network Related Utilities',
+      author='Diarmuid Collins',
+      author_email='dcollins@curtisswright.com',
+      url='https://github.com/diarmuidcwc/AcraNetwork',
+      packages=['IENA', 'iNetX','SimpleEthernet','Pcap','ParserAligned'],
+     )
