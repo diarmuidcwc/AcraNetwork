988a525707b5aa7791671dcbce5ed22bb2ef54f1
Diarmuid Collins <dcollins@curtisswright.com>
2014-03-18 12:16:00 +0000

    Updated the README and setup.py for pypi

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ from distutils.core import setup
 
 setup(name='AcraNetwork',
       version='0.1',
-      description='Acra Network Related Utilities',
+      description='Classes and utilities to support Flight Test Instrumentation Ethernet networks',
       author='Diarmuid Collins',
       author_email='dcollins@curtisswright.com',
       url='https://github.com/diarmuidcwc/AcraNetwork',
       py_modules=['IENA', 'iNetX','SimpleEthernet','Pcap','ParserAligned'],
+      classifiers =['Programming Language :: Python',
+                    'License :: OSI Approved :: Apache Software License',
+                    'Operating System :: OS Independent',
+                    'Development Status :: 3 - Alpha',
+                    'Intended Audience :: Developers',
+                    'Topic :: Software Development :: Libraries :: Python Modules',
+                    ],
      )
