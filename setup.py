2a3efdbc529123db53d7c82eacefa0494f1b6bcd
Douglas Kastle <douglas.kastle@gmail.com>
2015-07-31 16:12:59 +0100

    Added subdirectories

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='AcraNetwork',
-      version='0.3',
+      version='0.4',
       description='Classes and utilities to support Flight Test Instrumentation Ethernet networks',
       author='Diarmuid Collins',
       author_email='dcollins@curtisswright.com',
       url='https://github.com/douglaskastle/AcraNetwork',
-      packages=['AcraNetwork'],
+      packages=['AcraNetwork', 'AcraNetwork/test', 'AcraNetwork/protocols/network'],
       long_description=open('README.md', 'rt').read(),
       classifiers =['Programming Language :: Python',
                     'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
