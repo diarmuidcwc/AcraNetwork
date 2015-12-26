70485294369668cdd46c9c09425fe2571c2804f7
Diarmuid Collins <diarmuid.m.collins@gmail.com>
2015-12-26 09:03:17 +0000

    Coverted the README.md to normal txt for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import shutil
+
+try:
+    from pypandoc import convert
+    output = convert('README.md', 'rst', outputfile="README.txt")
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    shutil.copyfile("README.md","README.txt")
 
 setup(name='AcraNetwork',
-      version='0.4',
+      version='0.5',
       description='Classes and utilities to support Flight Test Instrumentation Ethernet networks',
       author='Diarmuid Collins',
       author_email='dcollins@curtisswright.com',
       url='https://github.com/diarmuidcwc/AcraNetwork',
       packages=['AcraNetwork'],
-      long_description=open('README', 'rt').read(),
+      long_description=open('README.txt', 'rt').read(),
       classifiers =['Programming Language :: Python',
                     'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
                     'Operating System :: OS Independent',
