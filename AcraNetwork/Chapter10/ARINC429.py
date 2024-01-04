4896e0ba112a702d5d9280efc3a9f10c258a267e
Diarmuid Collins <dcollins@curtisswright.com>
2024-01-04 16:10:48 +0000

    # 0.16.15 - fixed bug on ARINC conversion

--- AcraNetwork/Chapter10/ARINC429.py
+++ AcraNetwork/Chapter10/ARINC429.py
@@ -1,6 +1,80 @@
 import struct
 
 
+class ARINC429DataWord(object):
+    """
+    The Chapter 10 standard defines specific payload formats for different data. This class handles AROINC-429 packets
+
+    :type msgcount: int
+    :type gaptime: int
+    :type format_error: bool
+    :type parity_error: bool
+    :type bus_speed: int
+    :type bus: int
+    :type payload: str
+    """
+
+    LO_SPEED = 0  #: Bus speed constant
+    HI_SPEED = 1  #: Bus speed constant
+
+    HDR_FORMAT = ">HBB"
+
+    def __init__(self):
+        self.gaptime = 0  #: The gap time from the beginning of the preceding bus word (regardless of bus) to the
+        # beginning of the current bus word in 0.1-us increments
+        self.format_error = False  #: Format error has occurred
+        self.parity_error = False  #: Parity error has occurred
+        self.bus_speed = ARINC429DataWord.LO_SPEED  #: Arinc bus speed
+        self.bus = None  #: Bus number index from 0
+        self.payload = b""  #: ARINC word as a string payload
+
+    def pack(self):
+        """
+        Pack the ARINC-429 data packet object into a binary buffer
+
+        :rtype: str|bytes
+        """
+        _flag = (self.format_error << 7) + (self.parity_error << 6) + (self.bus_speed << 5) + (self.gaptime >> 16)
+        _gap = self.gaptime & 0xFFFF
+        hdr = struct.pack(ARINC429DataWord.HDR_FORMAT, _gap, _flag, self.bus)
+
+        return hdr + self.payload
+
+    def unpack(self, buffer):
+        """
+        Unpack a string buffer into an ARINC-429 data packet object
+
+        :param buffer: A string buffer representing an ARINC-429 data  packet
+        :type buffer: str
+        :rtype: None
+        """
+        (_gap, _flag, self.bus) = struct.unpack_from(ARINC429DataWord.HDR_FORMAT, buffer)
+        self.payload = buffer[struct.calcsize(ARINC429DataWord.HDR_FORMAT) :]
+        self.format_error = bool((_flag >> 7) & 0x1)
+        self.parity_error = bool((_flag >> 6) & 0x1)
+        self.bus_speed = (_flag >> 6) & 0x1
+        self.gaptime = (_flag << 16) + _gap
+
+        return True
+
+    def __eq__(self, other):
+        if not isinstance(other, ARINC429DataWord):
+            return False
+
+        _match_att = ("gaptime", "format_error", "parity_error", "bus_speed", "bus", "payload")
+
+        for attr in _match_att:
+            if getattr(self, attr) != getattr(other, attr):
+                return False
+
+        return True
+
+    def __repr__(self):
+        return "ARINCData: GapTime={} FormatError={} ParityError={} BusSpeed={} Bus={}".format(
+            self.gaptime, self.format_error, self.parity_error, self.bus_speed, self.bus
+        )
+
+
 class ARINC429DataPacket(object):
     """
     Data Packet Format. Contains a list of Arinc Data Words
@@ -85,7 +159,7 @@ class ARINC429DataPacket(object):
         self._index = 0
         return self
 
-    def next(self):
+    def next(self) -> ARINC429DataWord:
         if self._index < len(self.arincwords):
             _dw = self.arincwords[self._index]
             self._index += 1
@@ -100,77 +174,3 @@ class ARINC429DataPacket(object):
 
     def __getitem__(self, key):
         return self.arincwords[key]
-
-
-class ARINC429DataWord(object):
-    """
-    The Chapter 10 standard defines specific payload formats for different data. This class handles AROINC-429 packets
-
-    :type msgcount: int
-    :type gaptime: int
-    :type format_error: bool
-    :type parity_error: bool
-    :type bus_speed: int
-    :type bus: int
-    :type payload: str
-    """
-
-    LO_SPEED = 0  #: Bus speed constant
-    HI_SPEED = 1  #: Bus speed constant
-
-    HDR_FORMAT = ">HBB"
-
-    def __init__(self):
-        self.gaptime = 0  #: The gap time from the beginning of the preceding bus word (regardless of bus) to the
-        # beginning of the current bus word in 0.1-us increments
-        self.format_error = False  #: Format error has occurred
-        self.parity_error = False  #: Parity error has occurred
-        self.bus_speed = ARINC429DataWord.LO_SPEED  #: Arinc bus speed
-        self.bus = None  #: Bus number index from 0
-        self.payload = b""  #: ARINC word as a string payload
-
-    def pack(self):
-        """
-        Pack the ARINC-429 data packet object into a binary buffer
-
-        :rtype: str|bytes
-        """
-        _flag = (self.format_error << 7) + (self.parity_error << 6) + (self.bus_speed << 5) + (self.gaptime >> 16)
-        _gap = self.gaptime & 0xFFFF
-        hdr = struct.pack(ARINC429DataWord.HDR_FORMAT, _gap, _flag, self.bus)
-
-        return hdr + self.payload
-
-    def unpack(self, buffer):
-        """
-        Unpack a string buffer into an ARINC-429 data packet object
-
-        :param buffer: A string buffer representing an ARINC-429 data  packet
-        :type buffer: str
-        :rtype: None
-        """
-        (_gap, _flag, self.bus) = struct.unpack_from(ARINC429DataWord.HDR_FORMAT, buffer)
-        self.payload = buffer[struct.calcsize(ARINC429DataWord.HDR_FORMAT) :]
-        self.format_error = bool((_flag >> 7) & 0x1)
-        self.parity_error = bool((_flag >> 6) & 0x1)
-        self.bus_speed = (_flag >> 6) & 0x1
-        self.gaptime = (_flag << 16) + _gap
-
-        return True
-
-    def __eq__(self, other):
-        if not isinstance(other, ARINC429DataWord):
-            return False
-
-        _match_att = ("gaptime", "format_error", "parity_error", "bus_speed", "bus", "payload")
-
-        for attr in _match_att:
-            if getattr(self, attr) != getattr(other, attr):
-                return False
-
-        return True
-
-    def __repr__(self):
-        return "ARINCData: GapTime={} FormatError={} ParityError={} BusSpeed={} Bus={}".format(
-            self.gaptime, self.format_error, self.parity_error, self.bus_speed, self.bus
-        )
