cc4558e2ebeb1c9820e7f4a85b3199b52b970b9e
Diarmuid Collins <dcollins@curtisswright.com>
2025-05-28 14:33:27 +0100

    Added mechanism to install without building the C extension

--- setup.py
+++ setup.py
@@ -1,16 +1,38 @@
 from setuptools import setup, Extension
-import os
+from setuptools.command.build_ext import build_ext
 
-# Define the C extension
-golay_extension = Extension(
-    "AcraNetwork.IRIG106.Chapter7.golay_c",
-    sources=["AcraNetwork/IRIG106/Chapter7/golay_c.c"],
-    include_dirs=[],
-    extra_compile_args=[],
-    language="c",
-)
+
+class OptionalBuildExt(build_ext):
+    """Allow C extension building to fail without aborting the install."""
+
+    def run(self):
+        try:
+            super().run()
+        except Exception:
+            print("C extension build failed, using Python fallback.")
+
+    def build_extension(self, ext):
+        try:
+            super().build_extension(ext)
+        except Exception:
+            print(f"Building extension {ext.name} failed, skipping.")
+
+
+ext_modules = []
+try:
+    ext_modules.append(
+        Extension(
+            "AcraNetwork.IRIG106.Chapter7.golay_c",
+            ["AcraNetwork/IRIG106/Chapter7/golay_c.c"],
+            language="c",
+        )
+    )
+except Exception:
+    pass  # Ignore if setup fails to define extension
+
 
 # Load configuration from setup.cfg
 setup(
-    ext_modules=[golay_extension],
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": OptionalBuildExt},
 )
